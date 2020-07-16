a11ebb70404d3d852ad86eda426f7f6f3ae64114
videowala <51438045+videowala@users.noreply.github.com>
2020-07-16 12:13:29 +0530

    Update config.py

--- moviepy/config.py
+++ moviepy/config.py
@@ -14,7 +14,7 @@ try:
 except ImportError:
     DOTENV = None
 
-FFMPEG_BINARY = os.getenv("FFMPEG_BINARY", "/usr/bin/ffmpeg")
+FFMPEG_BINARY = os.getenv("FFMPEG_BINARY", "ffmpeg-imageio")
 IMAGEMAGICK_BINARY = os.getenv("IMAGEMAGICK_BINARY", "auto-detect")
 
 
